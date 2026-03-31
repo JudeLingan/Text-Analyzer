d8810c5cdb9a4cfb8dd7383820f3373a521bc22c
Jude Lingan <judecohenlingan@gmail.com>
2026-03-30 21:38:28 -0400

    added main function

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -6,17 +6,25 @@ import org.example.Analysis.Analysis;
 
 public class App {
     public static void main(String[] args) {
+		long startTime = System.currentTimeMillis();
 		try {
 			Analysis analysis = new Analysis(args[0]);
-			System.out.println(analysis.wordCount);
+			System.out.println(String.format("Word Count: %d\n", analysis.wordCount));
+
+			System.out.println("Most commmon words: ");
 			for (int i = 0; i < analysis.topWords.length; ++i) {
 				System.out.println(analysis.topWords[i]);
 			}
-			System.out.println(analysis.longest);
-			System.out.println(analysis.averageLength);
+			System.out.println();
+
+			System.out.println(String.format("Lengest word: %s\n", analysis.longest));
+			System.out.println(String.format("Average word length: %.2f\n", analysis.averageLength));
 		}
 		catch (FileNotFoundException e) {
 			System.out.println(e.getMessage());
 		}
+		long endTime = System.currentTimeMillis();
+		double timeSeconds = (double) (endTime - startTime) / 1000;
+		System.out.println(String.format("Execution time: %.3fs", timeSeconds));
     }
 }
